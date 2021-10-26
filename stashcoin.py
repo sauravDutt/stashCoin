59ba46c922a64df5a6a1c48b006fb3b3ee4d17c7
sauravDutt <sauravd069@gmail.com>
2021-10-26 14:56:24 +0530

    Just added some more stuff

--- stashcoin.py
+++ stashcoin.py
@@ -101,3 +101,38 @@ def main_block():
     previous_block = blockchain.print_previous_block()
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+
+    response = {
+        'message': 'A block is MINED',
+        'index': block['index'],
+        'timestamp': block['timestamp'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash']
+    }
+
+    return jsonify(response), 200
+
+# Display blockchain in json format
+@app.route('/get_chain', methods=['GET'])
+def display_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
+    }
+    return jsonify(response), 200
+
+# Check validity of blockchain
+@app.route('/valid', methods=['GET'])
+def valid():
+    valid = blockchain.chain_valid(blockchain.chain)
+
+    if valid:
+        response = {'message': 'The Blockchain is valid.'}
+    else:
+        response = {'message': 'The Blockchain is not valid.'}
+    return jsonify(response), 200
+
+# Run the flask server locally
+app.run(host = '127.0.0.1', port=5000)
