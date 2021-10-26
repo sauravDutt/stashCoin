3d3f5affe55c89fe950f2af71823925984ea0e0d
sauravDutt <sauravd069@gmail.com>
2021-10-26 13:31:39 +0530

    Just added some more stuff

---
+++ stashcoin.py
@@ -0,0 +1,27 @@
+import datetime
+
+import hashlib
+
+import json
+
+from flask import Flask, jsonify
+
+
+class Blockchain:
+
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash='0')
+    
+    def create_block(self, proof, previous_hash):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp' : str(datetime.datetime.now()),
+            'proof': proof,
+            'previous_hash': previous_hash
+        }
+
+        self.chain.append(block)
+        return block
+    
+    
